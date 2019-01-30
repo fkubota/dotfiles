8c62300a315078d0e6860713925282dd181b7048
fumihiro kubota <rkkubotay@gmail.com>
2019-01-31 08:12:28 +0900

    .vim, .vimrc を追加

---
+++ dots.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -u
+cd ~/dotfiles
+# 実行場所のディレクトリを取得
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+git submodule init
+git submodule update
+
+echo "start setup..."
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".require_oh-my-zsh" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    ln -snfv ~/dotfiles/"$f" ~/
+done
+
+[ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
+
+cat << END
+
+**************************************************
+DOTFILES SETUP FINISHED! bye.
+**************************************************
+
+END

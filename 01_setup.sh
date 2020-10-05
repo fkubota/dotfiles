d6d4f53019b21c5097feb649d2e7615b4e38e121
fkubota <rkkubotay@gmail.com>
2020-10-05 22:25:56 +0900

    [Update] vimrc, markdown-previewを削除

--- 01_setup.sh
+++ 01_setup.sh
@@ -10,7 +10,6 @@ sh ~/installer.sh ~/.cache
 # flake8のインストール
 pip3 install flake8 --user
 
-
 # tmux
 mkdir -p /home/fkubota/.tmux/theme/
 git clone https://github.com/arcticicestudio/nord-tmux.git ~/.tmux/themes/nord-tmux

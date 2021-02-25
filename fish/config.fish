b669439c63f36041223ab16a5364a04e654d31c0
fkubota <rkkubotay@gmail.com>
2021-02-25 13:51:27 +0900

    [update] config.fish, fzf history

--- fish/config.fish
+++ fish/config.fish
@@ -28,11 +28,9 @@ set -gx FZF_LEGACY_KEYBINDINGS 0
 set -gx FZF_CTRL_T_OPTS "--preview 'bat --color=always --style=header,grid --line-range :200 {}'"
 set -gx FZF_DEFAULT_OPTS "--height 80% --reverse --border"
 set -gx FZF_ALT_C_OPTS   "--preview 'tree -F -C {} | head -200'"
-# set -gx FZF_CTRL_T_OPTS "--preview 'bat --color \"always\" {}' --height 90%"
 function fish_user_key_bindings
   bind \cr __fzf_history
 end
-
 function __fzf_history
   history | fzf-tmux -d40% +s +m --tiebreak=index --query=(commandline -b) \
     > /tmp/fzf

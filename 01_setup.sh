a02a038420ee1d03776d0bcc2bed50a4764ef6d9
fkubota <rkkubotay@gmail.com>
2021-05-18 16:59:57 +0900

    [update]

--- 01_setup.sh
+++ 01_setup.sh
@@ -1,7 +1,6 @@
 # create directory
 mkdir ~/.vim 
 mkdir ~/.vim/rc 
-# mkdir ~/.vim/colors
 
 # dein のインストール
 curl https//raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh

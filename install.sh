f22da670470a5c54fcd474cecc86e1daa03ec911
fkubota <rkkubotay@gmail.com>
2021-05-19 18:59:19 +0900

    [update]

--- install.sh
+++ install.sh
@@ -22,15 +22,15 @@ git clone https://github.com/fkubota/dotfiles.git ${DOT_DIR}
 
 # vim
 mkdir -p ~/.vim/rc/
-ln -sf ~/Git/dotfiles/vim/.vimrc ~/.vimrc
-ln -sf ~/Git/dotfiles/vim/rc/dein.toml ~/.vim/rc/dein.toml
-ln -sf ~/Git/dotfiles/vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+ln -sf ${DOT_DIR}/vim/.vimrc ~/.vimrc
+ln -sf ${DOT_DIR}/vim/rc/dein.toml ~/.vim/rc/dein.toml
+ln -sf ${DOT_DIR}/vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 
 # tmux
-ln -sf ~/Git/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ${DOT_DIR}/tmux/.tmux.conf ~/.tmux.conf
 
 # fish
 mkdir -p ~/.config/fish/functions
-ln -sf ~/Git/dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -sf ~/Git/dotfiles/fish/config-osx.fish ~/.config/fish/config-osx.fish
-ln -sf ~/Git/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+ln -sf ${DOT_DIR}/fish/config.fish ~/.config/fish/config.fish
+ln -sf ${DOT_DIR}/fish/config-osx.fish ~/.config/fish/config-osx.fish
+ln -sf ${DOT_DIR}/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish

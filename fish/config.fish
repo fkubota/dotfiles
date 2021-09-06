47be4a653b42a57d8830b82ea83b5aaee5f34721
fkubota <rkkubotay@gmail.com>
2021-09-06 12:35:57 +0900

    [update] config.fish, fzf.fishを導入したのでconfig.fishのfzfをコメントアウト

--- fish/config.fish
+++ fish/config.fish
@@ -30,18 +30,18 @@ fish_default_key_bindings
 # end
 
 # fzf
-set -gx FZF_LEGACY_KEYBINDINGS 0
-set -gx FZF_CTRL_T_OPTS "--preview 'bat --color=always --style=header,grid --line-range :200 {}'"
-set -gx FZF_DEFAULT_OPTS "--height 80% --reverse --border"
-set -gx FZF_ALT_C_OPTS   "--preview 'tree -F -C {} | head -200'"
-function fish_user_key_bindings
-  bind \cr __fzf_history
-end
-function __fzf_history
-  history | fzf-tmux -d40% +s +m --tiebreak=index --query=(commandline -b) \
-    > /tmp/fzf
-  and commandline (cat /tmp/fzf)
-end
+# set -gx FZF_LEGACY_KEYBINDINGS 0
+# set -gx FZF_CTRL_T_OPTS "--preview 'bat --color=always --style=header,grid --line-range :200 {}'"
+# set -gx FZF_DEFAULT_OPTS "--height 80% --reverse --border"
+# set -gx FZF_ALT_C_OPTS   "--preview 'tree -F -C {} | head -200'"
+# function fish_user_key_bindings
+#   bind \cr __fzf_history
+# end
+# function __fzf_history
+#   history | fzf-tmux -d40% +s +m --tiebreak=index --query=(commandline -b) \
+#     > /tmp/fzf
+#   and commandline (cat /tmp/fzf)
+# end
 
 
 # The next line updates PATH for the Google Cloud SDK.
